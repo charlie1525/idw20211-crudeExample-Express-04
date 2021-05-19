2f35c300efce470ebfbfec3ef572ddbd287babe3
Carlos Uriel Fregoso Espericueta <caurfregosoes@ittepic.edu.mx>
2021-05-19 08:48:36 -0600

    Addin the fin and findOne method to the project

--- router/country.router.js
+++ router/country.router.js
@@ -11,6 +11,7 @@ mongoose.connect('mongodb://localhost:27017/countries', {
 const Country = require('../models/country.model');
 
 module.exports = () => {
+    //Incersión de paises
     router.post('/', (req, res) => {
         country = req.body;
         Country.create(country)
@@ -38,6 +39,67 @@ module.exports = () => {
                 }// fin de la funcion de error
             );
     });
+
+    router.get('/',(req,res)=>{
+        // Busqueda general
+        Country.find({})
+        .then(
+            (countries) =>{
+                res.json({
+                    code: status.OK,
+                    msj: 'Consulta correcta',
+                    data: countries
+                }) //EL json de todos los paises
+            }
+        ) //Fin del then
+        .catch(
+            (err)=>{
+                res.status(status.BAD_REQUEST)
+                .json({
+                    code: status.BAD_REQUEST,
+                    msg: 'Error en la petición',
+                    err: err.name,
+                    detail: err.message
+                })
+            } // fin del error
+        )// fin del catch
+
+    }); //fin del get 
+
+
+    router.get('/:id',(req,res)=>{
+        // Busqueda especifica con el ID
+        const id = req.params.id;
+
+        Country.findOne({_id : id})
+        .then(
+            (country) =>{
+                if(country)
+                res.json({
+                    code: status.OK,
+                    msj: 'Consulta correcta',
+                    data: country
+                }) //EL json de todos los paises
+                else 
+                res.json({
+                    code: status.NOT_FOUND,
+                    msj: 'No se encontro el elemento'
+                })
+            }
+        ) //Fin del then
+        .catch(
+            (err)=>{
+                res.status(status.BAD_REQUEST)
+                .json({
+                    code: status.BAD_REQUEST,
+                    msg: 'Error en la petición',
+                    err: err.name,
+                    detail: err.message
+                })
+            } // fin del error
+        )// fin del catch
+
+    }); //fin del get 
 
     return router;
 };
